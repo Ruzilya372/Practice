2ddc805a759c8d18f60dbe590ec60ac9c6075308
Ruzilya <ruzila.ramazanova2005@gmail.com>
2026-07-09 14:40:39 +0300

    Update teory-practice.py

--- Python/Part 2/01.Базовые коллекции - list (списки)/teory-practice.py
+++ Python/Part 2/01.Базовые коллекции - list (списки)/teory-practice.py
@@ -108,3 +108,56 @@ if found_worker in workers_list:
     print("Работник на месте!")
 else:
     print("Сотрудник не работает!")
+
+
+"""
+Задача "Гугл"
+Программисты постоянно гуглят ошибки и ищут уже готовый код, который можно использовать для своей программы, 
+чтобы не изобретать велосипед. Андрей поступил также и нашёл для своего проекта код, который должен находить 
+минимальное и максимальное числа в списке. Вот этот код:
+----------
+nums_list = []
+N = int(input('Кол-во чисел в списке: '))
+for _ in range(N):
+ num = int(input('Очередное число: '))
+ nums_list.append(num)
+
+maximum = 0
+minimum = -1
+
+for i in nums_list:
+ if maximum < i:
+   maximum = i
+ if minimum > i:
+   minimum = i
+
+print('Максимальное число в списке:', maximum)
+print('Минимальное число в списке:', minimum)
+----------
+
+Однако он столкнулся с проблемой. Если брать, к примеру, количество чисел 5, 
+то на тестах -1 -2 -3 -4 -5 и 1 2 3 4 5 программа выводит неверный результат. 
+"""
+
+nums_list = []
+N = int(input('Кол-во чисел в списке: '))
+for _ in range(N):
+    num = int(input('Очередное число: '))
+    nums_list.append(num)
+
+if nums_list:
+    maximum = nums_list[0]
+    minimum = nums_list[0]
+
+for i in nums_list:
+    if maximum < i:
+        maximum = i
+    if minimum > i:
+        minimum = i
+
+
+if nums_list == []:
+    print("В списке нет чисел")
+else:
+    print('Максимальное число в списке:', maximum)
+    print('Минимальное число в списке:', minimum)

3c61e978c550beec9a641746694521296f9dafc9
Ruzilya <ruzila.ramazanova2005@gmail.com>
2026-09-01 09:58:10 +0300

    Add files via upload

---
+++ Python/Module 20: Basic collections - tuples/03_players/main.py
@@ -0,0 +1,8 @@
+players = {
+    ("Ivan", "Volkin"): (10, 5, 13),
+    ("Bob", "Robbin"): (7, 5, 14),
+    ("Rob", "Bobbin"): (12, 8, 2)
+}
+
+result = [(name + scores) for name, scores in players.items()]
+print(result)

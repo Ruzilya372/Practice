415dff612e2423752a7aa21d08d22c7b941401e4
Ruzilya <ruzila.ramazanova2005@gmail.com>
2026-09-01 10:02:51 +0300

    Delete Python/List_comprehensions directory

--- Python/List_comprehensions/05_reversal/main.py
+++
@@ -1,9 +0,0 @@
-text = input("Введите строку: ")
-start = text.index("h")
-finish = text.rindex("h")
-
-text_catalog = text[start + 1:finish]
-
-reverse_text_catalog = text_catalog[::-1]
-
-print("Развернутая последовательнсоть между первым и последним h:", reverse_text_catalog)
--- Python/List_comprehensions/06_two_dimensional_list/main.py
+++
@@ -1,2 +0,0 @@
-catalog = [[first_element + second_element *4 for second_element in range(3)] for first_element in range(1,5)]
-print(catalog)
